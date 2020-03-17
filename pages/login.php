eb12c301f54e759ee5589940eabb2abfb08be54e
rfluj <61628792+rfluj@users.noreply.github.com>
2020-03-17 07:46:49 -0700

    edit
    working

--- pages/login.php
+++ pages/login.php
@@ -1,16 +1,14 @@
 
+
+
 <?php require_once '../configs/config.php' ?>
 
 <?php
 	if (!isset($_COOKIE[$name_cookie])) {
 		if (isset($_POST['register'])) {
-			$username = $_POST['username'];
-			$password = encrypt($_POST['password'], $key);
-			if (isset($_POST['remember_me'])) {
-				$remember_me = true;
-			} else {
-				$remember_me = false;
-			}
+			$username    = $_POST['username'];
+			$password    = encrypt($_POST['password'], $key);
+			$remember_me = isset($_POST['remember_me']);
 			if (empty($username) or empty($password)) {
 				echo "tamam field ha ra por konid.";
 			} else {
@@ -21,26 +19,37 @@
 					mysqli_query($db, "INSERT INTO users (username, password) VALUES ('$username', '$password')");
 					$query = mysqli_query($db, "SELECT * FROM users WHERE username='$username' AND password='$password'");
 					if ($query) {
-						$row = $query->fetch_assoc();
-						$id  = encrypt($row['id'], $key);
-						if ($remember_me) {
-							setcookie($name_cookie, $id, time()+$time_cookie, '/');
-							$_COOKIE[$name_cookie] = $id;
+						$row      = $query->fetch_assoc();
+						$id       = $row['id'];
+						$q        = mysqli_query($db, "SELECT * FROM token WHERE id='$id'");
+						if (mysqli_num_rows($q) == 1) {
+							$r    = $q->fetch_assoc();
+							$Uuid = $r['Uuid'];
+							if ($remember_me) {
+								setcookie($name_cookie, $Uuid, time()+$time_cookie, '/');
+								$_COOKIE[$name_cookie] = $Uuid;
+							} else {
+								setcookie($name_cookie, $Uuid, 0, '/');
+								$_COOKIE[$name_cookie] = $Uuid;
+							}
 						} else {
-							setcookie($name_cookie, $id, 0, '/');
-							$_COOKIE[$name_cookie] = $id;
+							$encoding = uniqid(uniqid(), true);
+							mysqli_query($db, "INSERT INTO token (id, Uuid) VALUES ('$id', '$encoding')");
+							if ($remember_me) {
+								setcookie($name_cookie, $encoding, time()+$time_cookie, '/');
+								$_COOKIE[$name_cookie] = $encoding;
+							} else {
+								setcookie($name_cookie, $encoding, 0, '/');
+								$_COOKIE[$name_cookie] = $encoding;
+							}
 						}
 					}
 				}
 			}
 		} elseif (isset($_POST['login'])) {
-			$username = $_POST['username'];
-			$password = encrypt($_POST['password'], $key);
-			if (isset($_POST['remember_me'])) {
-				$remember_me = true;
-			} else {
-				$remember_me = false;
-			}
+			$username    = $_POST['username'];
+			$password    = encrypt($_POST['password'], $key);
+			$remember_me = isset($_POST['remember_me2']);
 			if (empty($username) or empty($password)) {
 				echo "tamam field ha ra por konid.";
 			} else {
@@ -52,14 +61,36 @@
 					// $query = mysqli_query($db, "SELECT * FROM users ORDER BY id DESC LIMIT 1");
 					if ($query) {
 						$row = $query->fetch_assoc();
-						$id  = encrypt($row['id'], $key);
-						if ($remember_me) {
-							setcookie($name_cookie, $id, time()+$time_cookie, '/');
-							$_COOKIE[$name_cookie] = $id;
+						$id  = $row['id'];
+						$q   = mysqli_query($db, "SELECT * FROM token WHERE id='$id'");
+						if (mysqli_num_rows($q) == 0) {
+							$encoding = uniqid(uniqid(), true);
+							mysqli_query($db, "INSERT INTO token (id, Uuid) VALUES ('$id', '$encoding')");
+							if ($remember_me) {
+								setcookie($name_cookie, $encoding, time()+$time_cookie, '/');
+								$_COOKIE[$name_cookie] = $encoding;
+							} else {
+								setcookie($name_cookie, $encoding, 0, '/');
+								$_COOKIE[$name_cookie] = $encoding;
+							}
 						} else {
-							setcookie($name_cookie, $id, 0, '/');
-							$_COOKIE[$name_cookie] = $id;
+							$r    = $q->fetch_assoc();
+							$Uuid = $r['Uuid'];
+							if ($remember_me) {
+								setcookie($name_cookie, $Uuid, time()+$time_cookie, '/');
+								$_COOKIE[$name_cookie] = $Uuid;
+							} else {
+								setcookie($name_cookie, $Uuid, 0, '/');
+								$_COOKIE[$name_cookie] = $Uuid;
+							}
 						}
+					// 	if ($remember_me) {
+					// 		setcookie($name_cookie, $id, time()+$time_cookie, '/');
+					// 		$_COOKIE[$name_cookie] = $id;
+					// 	} else {
+					// 		setcookie($name_cookie, $id, 0, '/');
+					// 		$_COOKIE[$name_cookie] = $id;
+					// 	}
 					}
 				}
 			}
@@ -79,12 +110,32 @@
 <body>
 	<?php
 		if (isset($_COOKIE[$name_cookie])) {
-			$id = intval(decrypt($_COOKIE[$name_cookie], $key));
-			echo $id;
-			echo "<h1>wellcome username.</h1>
-				<form action=","./login.php"," method=","post",">
-					<input type=","submit"," name=","exit"," value=","exit",">
-				</form>";
+			$Uuid  = $_COOKIE[$name_cookie];
+			$query = mysqli_query($db, "SELECT * FROM token WHERE Uuid='$Uuid'");
+			if (mysqli_num_rows($query) == 0) {
+				unset($_COOKIE[$name_cookie]);
+				setcookie($name_cookie, false, time()-$time_cookie, '/');
+				header('location: ./login.php');
+				exit();
+			} else {
+				$row = $query->fetch_assoc();
+				$id  = $row['id'];
+				echo $id;
+				$query = mysqli_query($db, "SELECT * FROM users WHERE id='$id'");
+				if (mysqli_num_rows($query) == 0) {
+					echo "<h1>useri vojod nadarad.goyi mage?khar sabt nam haken.</h1>
+						<form action=","./login.php"," method=","post",">
+							<input type=","submit"," name=","exit"," value=","exit",">
+						</form>";
+				} else {
+					$row      = $query->fetch_assoc();
+					$username = $row['username'];
+					echo "<h1>wellcome $username.</h1>
+						<form action=","./login.php"," method=","post",">
+							<input type=","submit"," name=","exit"," value=","exit",">
+						</form>";
+				}
+			}
 		} else {
 			echo "<form action=","./login.php"," method=","post",">
 					<span>register</span>
@@ -106,7 +157,7 @@
 					<br>
 					<input type=","password"," name=","password"," placeholder=","password",">
 					<br>
-					<input type=","checkbox"," name=","remember_me"," id=","remember_me2",">
+					<input type=","checkbox"," name=","remember_me2"," id=","remember_me2",">
 					<label for=","remember_me2",">remember me</label>
 					<br>
 					<input type=","submit"," name=","login"," value=","login",">
